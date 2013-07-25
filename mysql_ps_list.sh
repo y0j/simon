43eacf62459ba667fb5f7abdc6737cb77c31a6a9
y0j <work.for.the.happiness@gmail.com>
2013-07-25 15:08:31 +0300

    Update mysql_ps_list.sh

--- mysql_ps_list.sh
+++ mysql_ps_list.sh
@@ -1,5 +1,5 @@
 #
-# Don't forget protect this file chmod/chown or some else
+# Don't forget protect this file by chmod/chown or some else
 
 #!/bin/sh
 /usr/bin/mysqladmin processlist -p<YOUR_PASS>
